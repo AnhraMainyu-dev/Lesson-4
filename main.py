226ccc2c304217d849d53f91ff7af724c39ec068
AnhraMainyu-dev <maximheilelov@gmail.com>
2026-01-16 14:44:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ from PIL import Image
 image = Image.open("monro.jpg")
 red_image, green_image, blue_image = image.convert("RGB").split()
 
-coordinates_1 = (50, 0, red_image.width, red_image.height)
-coordinates_2 = (25, 0, red_image.width-25, red_image.height)
+cropped_red_image_1 = red_image.crop((50, 0, red_image.width, red_image.height))
+cropped_red_image_2 = red_image.crop((25, 0, red_image.width-25, red_image.height))
+red_image = Image.blend(cropped_red_image_1, cropped_red_image_2, 0.5)
 
-cropped_red_image1 = red_image.crop(coordinates_1)
-cropped_red_image2 = red_image.crop(coordinates_2)
-red_blended_image = Image.blend(cropped_red_image1, cropped_red_image2, 0.7)
+cropped_blue_image_1 = blue_image.crop((0, 0, blue_image.width-50, blue_image.height))
+cropped_blue_image_2 = blue_image.crop((25, 0, blue_image.width-25, blue_image.height))
+blue_image = Image.blend(cropped_blue_image_1, cropped_blue_image_2, 0.5)
 
-cropped_blue_image1 = blue_image.crop(coordinates_1)
-cropped_blue_image2 = blue_image.crop(coordinates_2)
-blue_blended_image = Image.blend(cropped_blue_image1, cropped_blue_image2, 0.7)
+green_image = green_image.crop((25, 0, green_image.width-25, green_image.height))
 
-cropped_green_image1 = green_image.crop(coordinates_2)
-
-end_image = Image.merge("RGB", (red_blended_image, blue_blended_image, cropped_green_image1))
+end_image = Image.merge("RGB", (red_image, green_image, blue_image))
 end_image.thumbnail((80, 80))
 end_image.save("avatar.jpg")
+
